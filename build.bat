13aed8c5b541ae79582b8ecdefc676a367abc5e8
John-John Tedro <udoprog@spotify.com>
2018-04-16 14:50:34 +0200

    change build image and update script

--- build.bat
+++ build.bat
@@ -1,14 +1,14 @@
-@echo off
-
 set /p version=<build_version
+
+set exe=reproto.exe
+set "target=%cd%\target"
+set "bin=%target%\bin\%exe%"
+set "upload=%target%\upload"
 
 set "platform=windows"
 set "arch=x86_64"
 set "tuple=%version%-%platform%-%arch%"
-set "target=target"
-set "bin=%target%\bin\reproto"
-set "upload=%target%\upload"
-set "archive=%upload%\reproto-%tuple%.tar.gz"
+set "archive=reproto-%tuple%.tar.gz"
 set "git=https://github.com/reproto/reproto"
 
 echo Building: %tuple%
@@ -19,5 +19,7 @@ If NOT exist %bin% (
     Exit 1
 )
 
-7za.exe a -ttar -so archive.tar %bin% | 7za.exe a -si %archive%
+mkdir %upload%
+pushd %target%\bin
+7z a -ttar -so archive.tar %exe% | 7z a -si %upload%\%archive%
 Exit 0
